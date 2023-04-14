653ab768e8db7c433057a00185a856c0def9a435
jsosamarbill <julio.sosa@marbill.com>
2023-04-14 12:11:52 +0200

    Messing with double range

--- scripts.js
+++ scripts.js
@@ -85,6 +85,8 @@ rangeInput.forEach((input) => {
   input.addEventListener("input", (e) => {
     let minRange = parseInt(rangeInput[0].value);
     let maxRange = parseInt(rangeInput[1].value);
+    console.log('minRange', minRange);
+    console.log('maxRange', maxRange);
     if (maxRange - minRange < rangeMin) {     
       if (e.target.className === "min") {
         rangeInput[0].value = maxRange - rangeMin;        
@@ -95,8 +97,23 @@ rangeInput.forEach((input) => {
       rangePrice[0].value = minRange;
       rangePrice[1].value = maxRange;
       range.style.left = (minRange / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+    }
+  });
+});
 
-      range.style.right = 100 - (maxRange / rangeInput[1].max) * 90 + "%";
+rangePrice.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = rangePrice[0].value;
+    let maxPrice = rangePrice[1].value;
+    if (maxPrice - minPrice >= rangeMin && maxPrice <= rangeInput[1].max) {
+      if (e.target.className === "min") {
+        rangeInput[0].value = minPrice;
+        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+      }
     }
   });
 });
