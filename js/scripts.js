19c435ef5a31c09b7fc6ac56aa7be9176e2ee1fb
jsosamarbill <julio.sosa@marbill.com>
2023-06-08 17:07:08 -0400

    New selects, update style guide

--- js/scripts.js
+++ js/scripts.js
@@ -204,8 +204,78 @@ if (filterRangeOnPage) {
   })
 }
 
-if ($('.form__select-multiple-items select')) {
-  $('.form__select-multiple-items select').chosen();
+// if ($('.form__select-multiple-items select')) {
+//   $('.form__select-multiple-items select').chosen();
+// }
+
+
+class CustomSelect {
+  constructor(originalSelect) {
+    this.originalSelect = originalSelect;
+    this.customSelect = document.createElement("div");
+    this.customSelect.classList.add("traits-select");
+
+    this.originalSelect.querySelectorAll("option").forEach((optionElement) => {
+      const itemElement = document.createElement("div");
+
+      itemElement.classList.add("traits-select__item");
+      itemElement.textContent = optionElement.textContent;
+      this.customSelect.appendChild(itemElement);
+
+      if (optionElement.selected) {
+        this._select(itemElement);
+      }
+
+      itemElement.addEventListener("click", () => {
+        if (
+          this.originalSelect.multiple &&
+          itemElement.classList.contains("traits-select__item--selected")
+        ) {
+          this._deselect(itemElement);
+        } else {
+          this._select(itemElement);
+        }
+      });
+    });
+
+    this.originalSelect.insertAdjacentElement("afterend", this.customSelect);
+    this.originalSelect.style.display = "none";
+  }
+
+  _select(itemElement) {
+    const index = Array.from(this.customSelect.children).indexOf(itemElement);
+
+    if (!this.originalSelect.multiple) {
+      this.customSelect.querySelectorAll(".traits-select__item").forEach((el) => {
+        el.classList.remove("traits-select__item--selected");
+      });
+    }
+
+    this.originalSelect.querySelectorAll("option")[index].selected = true;
+    itemElement.classList.add("traits-select__item--selected");
+  }
+
+  _deselect(itemElement) {
+    const index = Array.from(this.customSelect.children).indexOf(itemElement);
+
+    this.originalSelect.querySelectorAll("option")[index].selected = false;
+    itemElement.classList.remove("traits-select__item--selected");
+  }
 }
 
+document.querySelectorAll(".custom-select").forEach((selectElement) => {
+  new CustomSelect(selectElement);
+});
 
+document.querySelectorAll(".gender-select").forEach((selectElement) => {
+  const genderSelect = selectElement.nextSibling;
+  genderSelect.classList.add('gender-select');
+  const genderSelectItems = genderSelect.querySelectorAll('.traits-select__item');
+  const selectOptions = selectElement.options;
+  
+  for (var i =0; i < selectOptions.length; i++) {
+    console.log(selectOptions[i].dataset.value);
+    genderSelectItems[i].dataset.value = selectOptions[i].dataset.value;
+    genderSelectItems[i].innerHTML = `<span>${selectOptions[i].dataset.value}</span>`;
+  }
+});
